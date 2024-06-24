0fd386743eb8ccb0550e20c8887f565eeb339716
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-06-24 16:25:13 -0700

    fix: pipefail err

--- oi_runner/src/lib.rs
+++ oi_runner/src/lib.rs
@@ -210,7 +210,7 @@ pub async fn download_rye(client: &Client) -> anyhow::Result<()> {
     };
 
     if bin_exists("gunzip")? && bin_exists("curl")? {
-        let o = Command::new(SHELL).args(["-c", "curl -sSf https://rye.astral.sh/get | RYE_TOOLCHAIN_VERSION=\"3.11.9\" RYE_INSTALL_OPTION=\"--yes\" sh"]).output()?;
+        let o = Command::new(SHELL).args(["-c", "curl -sSf https://rye.astral.sh/get | RYE_TOOLCHAIN_VERSION=\"3.11.9\" RYE_INSTALL_OPTION=\"--yes\" bash"]).output()?;
 
         info!("rye curl installer resp | {o:#?}");
 
